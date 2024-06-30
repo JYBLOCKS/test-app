43416ffb605e46032375e2d7f314a4c71c646e77
José Enrique Arredondo Herrera <joseherrecr1996@gmail.com>
2024-06-29 23:21:38 -0600

    feature you will see the coverage in pipelines jobs

--- jest.config.ts
+++ jest.config.ts
@@ -3,8 +3,16 @@ export default {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-   moduleNameMapper: {
-     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
 };
